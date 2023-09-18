eb8d152f9a78628d72a0b529df702beb2300f1b4
Rongchuansun <116932069+Rongchuansun@users.noreply.github.com>
2023-09-18 13:29:02 +0800

    Add files via upload

--- ReadSensor/test_sensor.py
+++ ReadSensor/test_sensor.py
@@ -1,49 +1,47 @@
 import time
-from gpiozero import Button
+from gpiozero import DistanceSensor
 
-def readFromSensor():     
-    # Initialize the sensors (buttons)
-    geton_sensor = Button(17)   # Connect the boarding sensor to GPIO 17
-    getoff_sensor = Button(18)  # Connect the alighting sensor to GPIO 18
+# Initialize the sensors (ultrasonic sensors)
+geton_sensor = DistanceSensor(echo=17, trigger=4)  # Connect the boarding sensor to GPIO 17 (echo) and GPIO 4 (trigger)
+getoff_sensor = DistanceSensor(echo=18, trigger=5)  # Connect the alighting sensor to GPIO 18 (echo) and GPIO 5 (trigger)
 
-    # Initialize variables
-    current_passenger_count = 0
-    record_interval = 15  # Record data every 15 seconds
-    start_time = time.time()
-    time_list = []  # List to store time data
-    passenger_count_list = []  # List to store passenger count data
+# Initialize variables
+current_passenger_count = 0
+record_interval = 15  # Record data every 15 seconds
+start_time = time.time()
+time_list = []  # List to store time data
+passenger_count_list = []  # List to store passenger count data
 
-    try:
-        while True:
-            # Listen for sensor events
-            geton_sensor.wait_for_press()
+try:
+    while True:
+        # Listen for sensor events
+        geton_distance = geton_sensor.distance
+        if geton_distance <= 1.0:  # Set the threshold to 1 meter
             current_passenger_count += 1
-            getoff_sensor.wait_for_press()
+
+        getoff_distance = getoff_sensor.distance
+        if getoff_distance <= 1.0:  # Set the threshold to 1 meter
             current_passenger_count -= 1
 
-            # Check if the recording interval has been reached
-            elapsed_time = time.time() - start_time
-            if elapsed_time >= record_interval:
-                # Record the event
-                current_time = time.strftime("%H:%M:%S", time.localtime())
-                #print(f"Time: {current_time}, Passenger Count: {current_passenger_count}")
+        # Check if the recording interval has been reached
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= record_interval:
+            # Record the event
+            current_time = time.strftime("%H:%M:%S", time.localtime())
 
-                # Add time and passenger count to the lists
-                time_list.append(current_time)
-                passenger_count_list.append(current_passenger_count)
+            # Add time and passenger count to the lists
+            time_list.append(current_time)
+            passenger_count_list.append(current_passenger_count)
 
-                # Reset the timer and passenger count
-                start_time = time.time()
-                #current_passenger_count = 0
+            # Reset the timer and passenger count
+            start_time = time.time()
 
-    except KeyboardInterrupt:
-        pass
+except KeyboardInterrupt:
+    pass
 
-    # Output the results
-    print("Time List:", time_list)
-    print("Passenger Count List:", passenger_count_list)
-
-    return time_list, passenger_count_list
+# Output the results
+print("Time List:", time_list)
+print("Passenger Count List:", passenger_count_list)
 
 
 
